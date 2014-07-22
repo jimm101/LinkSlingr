4bab39b8cecc25171ff0a38497eb9e6c8332340a
Jim McGowan <jimmcgowan@Jims-MacBook-Pro.local>
2014-07-21 22:42:17 -0400

    refactored client handling in server

--- server/LinkSlingd.js
+++ server/LinkSlingd.js
@@ -1,30 +1,39 @@
 var clients = [];
 var timers = {};
+var connection_count = 0;
 
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port: 7000});
 wss.on('connection', function(ws) {
-    console.log('connections:'+clients.length);
-    clients.push(ws);
-    timers[ws] = setInterval(function() { ws.send("ping"); }, 3000);
+    connection_count += 1;
+    console.log('connections:'+connection_count);
+    ws.id = Math.random() * 0x400000000;
+    clients[ws.id] = ws;
+    timers[ws.id] = setInterval(function() {
+      if( ws.readyState == 1 ) { /* web socket open */
+        ws.send("ping");
+      } else {
+        console.log('skipping');
+      }
+    }, 3000);
     ws.on('message', function(message) {
       switch( message ) {
         case 'pong':
           break;
         default:
-          clients.forEach( function (client) {
-            if( client != this ) {
-              client.send(message);
+          for( var id in clients ) {
+            if( clients.hasOwnProperty(id) && clients[id] != this ) {
+              clients[id].send(message);
             }
-          }.bind(ws));
+          };
       }
     });
     ws.on('close', function(code) {
-        index = clients.indexOf(ws);
-        clients.splice(index,1);
-        clearTimeout(timers[this]);
-        delete timers[this];
-        console.log('closed connection:'+code);
+        clearTimeout(timers[ws.id]);
+        delete timers[ws.id];
+        delete clients[ws.id];
+        connection_count -= 1;
+        console.log('closed connection:'+connection_count);
     });
     ws.send('connected');
 });
